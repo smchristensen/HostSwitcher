2f9734436fdebc95a92de8dfb2724a5793c7254b
Scott Christensen <schristensen@ruralins.com>
2015-08-07 08:39:49 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # HostSwitcher
-https://www.simple-talk.com/dotnet/.net-framework/creating-tray-applications-in-.net-a-practical-guide/
+
+I made a few modifications to the excellent code for creating a tray host switching application found [here](https://www.simple-talk.com/dotnet/.net-framework/creating-tray-applications-in-.net-a-practical-guide/).
+I wanted to be able to see the current host entry at a glance.  I first tried using the hover help but found it was limited to a specific text length.  My host file was getting fairly large so I
+ended up modifying the popup window to include the current entry instead.
---
+++ WpfFormLibrary/IntroForm.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+
+namespace WpfFormLibrary
+{
+    /// <summary>
+    /// Interaction logic for WpfForm1.xaml
+    /// </summary>
+    public partial class IntroForm : Window
+    {
+        public IntroForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+
+            e.Handled = true;
+
+        }
+    }
+}
